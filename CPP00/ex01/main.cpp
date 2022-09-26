e70ea26eea98616f87b6f3b7bae808c4001505c6
juzoanya <juzoanya@student.42wolfsburg.de>
2022-09-26 15:38:02 +0100

    tabs updated

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,28 +1,28 @@
 
 #include "PhoneBook.hpp"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    std::string     buff;
-    bool            run = true;
+	std::string	buff;
+	bool		run = true;
 
-    (void)argv;
-    if (argc != 1)
-        return (1);
-    while (run)
-    {
-        std::cout << "> ";
-        if (!std::getline(std::cin, buff))
-            return (0);
-        if (buff == "ADD")
-            std::cout << "Let's add something new here!" << std::endl;
-        if (buff == "SEARCH")
-            std::cout << "Let's Search something new here!" << std::endl;
-        if (buff == "EXIT")
-        {
-             std::cout << "Programme Terminated!" << std::endl;
-             run = false;
-        }
-    }
-    return (0);
+	(void)argv;
+	if (argc != 1)
+		return (1);
+	while (run)
+	{
+		std::cout << "> ";
+		if (!std::getline(std::cin, buff))
+			return (0);
+		if (buff == "ADD")
+			std::cout << "Let's add something new here!" << std::endl;
+		if (buff == "SEARCH")
+			std::cout << "Let's Search something new here!" << std::endl;
+		if (buff == "EXIT")
+		{
+			std::cout << "Programme Terminated!" << std::endl;
+			run = false;
+		}
+	}
+	return (0);
 }

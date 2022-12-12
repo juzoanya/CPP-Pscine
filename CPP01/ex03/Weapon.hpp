2cb5f18a7b9a9d0d4e610e26781f877e4c5f9d03
juzoanya <juzoanya@student.42wolfsburg.de>
2022-12-12 21:58:41 +0100

    updated

---
+++ CPP01/ex03/Weapon.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 20:46:43 by juzoanya          #+#    #+#             */
+/*   Updated: 2022/12/12 20:51:16 by juzoanya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+
+class Weapon
+{
+	private:
+		std::string	type;
+
+	public:
+		Weapon();
+		~Weapon();
+
+		void	setType(std::string type);
+}
+
+#endif

4d62294484ccd8a5e45eba154cc4a088c04e7330
juzoanya <juzoanya@student.42wolfsburg.de>
2022-12-12 19:50:50 +0100

    headers added

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,12 +1,22 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 19:49:51 by juzoanya          #+#    #+#             */
+/*   Updated: 2022/12/12 19:49:55 by juzoanya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
 
 int	main(void)
 {
 	std::string	str = "HI THIS IS BRAIN";
-	std::string	*stringPTR;
-	std::string	stringREF;
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
 
 
 }

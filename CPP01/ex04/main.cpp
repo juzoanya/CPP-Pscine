ac18ceb8ae583faf420a6de88aeeaf29087c3de1
juzoanya <juzoanya@student.42wolfsburg.de>
2023-01-17 16:07:29 +0100

    sed ex

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,19 +6,48 @@
 /*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:43:45 by juzoanya          #+#    #+#             */
-/*   Updated: 2023/01/12 16:24:08 by juzoanya         ###   ########.fr       */
+/*   Updated: 2023/01/17 15:48:12 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 
 int	stringReplace(std::string file, std::string s1, std::string s2)
 {
-	std::ifstream	inputFile;
-	std::ofstream	outputFile;
+	std::size_t		pos;
+	std::size_t		spos;
+	std::string		buffer;
+	std::string		newStr;
+	std::ifstream	inputFile(file, std::ios_base::in);
+	std::ofstream	outputFile(file.append(".replace"));
 
+	if (!inputFile.is_open())
+	{
+		std::cerr << "Error: failed to open " << file << std::endl;
+		return (1);
+	}
+	if (!outputFile.is_open())
+	{
+		std::cerr << "Error: failed to create " << file.append(".replace") << std::endl;
+		return (1);
+	}
+	spos = 0;
+	while (std::getline(inputFile, buffer))
+	{
+		while ((pos = buffer.find(s1)) != std::string::npos)
+		{
+			buffer.erase(spos, s1.size());
+			buffer.insert(spos, s2);
+		}
+		outputFile << buffer;
+		if (inputFile.eof())
+			outputFile << std::endl;
+	}
+	inputFile.close();
+	outputFile.close();
 	return (0);
 }
 

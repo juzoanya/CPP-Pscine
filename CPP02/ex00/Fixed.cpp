56c5be84bd938f7cdde6aff4aca904a472b197d0
juzoanya <juzoanya@student.42wolfsburg.de>
2023-01-26 17:05:43 +0100

    cpp02

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: juzoanya <juzoanya@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:53:14 by juzoanya          #+#    #+#             */
-/*   Updated: 2023/01/25 16:57:54 by juzoanya         ###   ########.fr       */
+/*   Updated: 2023/01/26 16:49:25 by juzoanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ Fixed::Fixed(void)
 
 Fixed::~Fixed(void)
 {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& nbr): _nbr(0)
+{
+	//this->_nbr = 0;
+	std::cout << "Copy constructor called" << std::endl;
+	*this = nbr;
+}
+
+Fixed& Fixed::operator=(const Fixed& nbr)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+
 }
